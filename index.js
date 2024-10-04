bc58a7be341806c3b2f15d1404ea31d70a0dd530
elliott <130366417+toorques@users.noreply.github.com>
2024-10-04 14:59:16 +0000

    criadas as rota home e sobre

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express();
 const PORT = 3300;
 
 app.get("/", function (req, res) {
-  // res.send("Olá...!");
-  res.send(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// Rota Utilizando middleware(404)
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/404.html");
 });
 app.listen(PORT, () => {
   console.log("running. . .");
